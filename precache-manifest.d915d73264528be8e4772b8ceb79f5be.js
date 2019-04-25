03133e4fd6aee6bf98694bb6e71a8e9fa68ac080
ysaini3 <yogesh.saini@myqsoft.com>
2019-04-25 17:45:07 +0530

    Updates

---
+++ precache-manifest.d915d73264528be8e4772b8ceb79f5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4feaf2edd2c045fc7f915439aaa1a0a2",
+    "url": "/rent-care-admin/index.html"
+  },
+  {
+    "revision": "d8b613955bad776facbf",
+    "url": "/rent-care-admin/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/rent-care-admin/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d8b613955bad776facbf",
+    "url": "/rent-care-admin/static/js/main.967ff400.chunk.js"
+  },
+  {
+    "revision": "1bde261ff6036abb20e7",
+    "url": "/rent-care-admin/static/js/runtime~main.697f83c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rent-care-admin/static/media/logo.5d5d9eef.svg"
+  }
+]);
