26c4dfc5804af65018df1f7db1016a3915b08f55
ysaini3 <yogesh.saini@myqsoft.com>
2019-05-14 16:16:22 +0530

    b

--- src/components/buyer-section/index.jsx
+++ src/components/buyer-section/index.jsx
@@ -1,20 +1,67 @@
 import React, { Component } from "react";
 import Header from "./header";
-import SmallBoxes from "./smallBoxes";
-import Container from "./container";
-import { loadInit } from "../../utility/loadInit";
+import Table from "../common/table";
+import { propertyTypes } from "../../utility/common";
+import { getBuyersProperties } from "../../services/adminServices";
+import { getTableHeader } from "../../utility/tableHeaders";
+import PropertySelect from "../dashboard/propertySelect";
 class BuyerSection extends Component {
+  state = {
+    propertyList: [],
+    filterPropertyList: [],
+    currentSelectProperty: "pg",
+    tableKeys: [],
+    propertyTypes
+  };
   componentDidMount() {
-    loadInit();
+    this.propertiesList();
+    // loadInit();
   }
+  propertiesList = async () => {
+    const data = {};
+    const propertyList = await getBuyersProperties(data);
+    this.setState({ propertyList });
+    this.onFilterProperty(this.state.currentSelectProperty);
+  };
+  onFilterProperty = type => {
+    const filterPropertyList = this.state.propertyList.filter(
+      item => item.Property === type
+    );
+    let tableKeys = getTableHeader(type).filter(
+      item =>
+        item !== "_id" &&
+        item !== "MarkAsRead" &&
+        item !== "Publish" &&
+        item !== "ShowAtHome"
+    );
+    tableKeys = ["BName", "BEmail", "BMobile", ...tableKeys];
+    this.setState({
+      tableKeys,
+      filterPropertyList,
+      currentSelectProperty: type
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         {/* Main content */}
         <section className="content">
           <Header />
-          <SmallBoxes />
-          <Container />
+          {/* <SmallBoxes /> */}
+          <PropertySelect
+            propertyTypes={this.state.propertyTypes}
+            onFilterProperty={this.onFilterProperty}
+          />
+          <Table
+            tableTitle={"All Buyer Properties Requests"}
+            tableKeys={this.state.tableKeys}
+            filterPropertyList={this.state.filterPropertyList}
+            onPublish={this.onPublish}
+            onShowAtHome={this.onShowAtHome}
+            onMarkAsRead={this.onMarkAsRead}
+          />
+          {/* <Container /> */}
         </section>
         {/* /.content */}
       </React.Fragment>
--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -7,9 +7,8 @@ export const getProperties = async data => {
   return await httpService("POST", "/property/search", data);
 };
 export const updateProperty = async data => {
-  /*let headers = {
-    "x-auth-token":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2JlYjE3Y2MyNzA4NjI5YmY4MmZiYTYiLCJuYW1lIjoiYWRhc2QiLCJlbWFpbCI6ImFZb2d5YSIsInVzZXJUeXBlIjoiYWRtaW4iLCJpYXQiOjE1NTYwMDMzMjR9.jE4KBW47xUivFPcmesX2J-t04wvEykYXgD2lKhTIauU"
-  };*/
   return await httpService("PUT", "/property/updateproperty", data);
+};
+export const getBuyersProperties = async data => {
+  return await httpService("POST", "/buyer/search", data);
 };
